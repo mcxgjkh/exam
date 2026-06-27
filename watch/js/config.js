e396a367f7c179cd279a9da70c3c5e49895cb395
LuccaWang404 <luccawang@404cafe.fun>
2026-06-27 08:48:39 +0800

    Version & EULA Update

--- watch/js/config.js
+++ watch/js/config.js
@@ -1,5 +1,5 @@
 // config.js
-export const VERSION = '3.12.10.20260623_beta.1';
+export const VERSION = '3.13.0.20260627_rc.1';
 
 export const EXAM_STANDARDS = {
   A: { total: 40, time: 40, pass: 30 },
